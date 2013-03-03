daca76319cb8ddeb6253c51b44ecce3ff2d794b2
ben <test@ex.com>
2013-03-03 21:39:37 +0100

    Version fonctionnelle
    Chef lance les installations de base et Configure les Vhosts pour Apache

---
+++ vagrant_main_test/recipes/default.rb
@@ -0,0 +1,44 @@
+require_recipe "apt"
+require_recipe "apache2"
+
+#Installation des basiques
+
+package "vim" do
+  action :install
+end
+
+package "php5" do
+  action :install
+end
+
+package "php5-cli" do
+  action :install
+end
+
+package "php5-dev" do
+  action :install
+end
+
+package "php5-mysql" do
+  action :install
+end
+
+package "mysql-server" do
+  action :install
+end
+
+package "mysql-client-core-5.1" do
+  action :install
+end
+
+#Fin des installations
+
+execute "disable-default-site" do
+  command "sudo a2dissite default"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
+
+web_app "project" do
+  template "project.conf.erb"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
